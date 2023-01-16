9db0d8bfbe35621e7549b1da442e9f2c3ac1f24a
ViolinDzhanov <122802081+ViolinDzhanov@users.noreply.github.com>
2023-01-16 19:17:17 +0000

    gues the number game code added

---
+++ Gues the number.cs
@@ -0,0 +1,28 @@
+﻿Random randomNumber = new Random();
+int computerNumber = randomNumber.Next(1, 101);
+
+while (true)
+{
+    Console.Write("Gues the nimber 1 - 100: ");
+    string playerInput = Console.ReadLine();
+    bool isValid = int.TryParse(playerInput, out int playernumber);
+    if (isValid)
+    {
+        if (playernumber == computerNumber)
+        {
+            Console.WriteLine("You guessed it!");
+        }
+        else if (playernumber > computerNumber)
+        {
+            Console.WriteLine("Too high!");
+        }
+        else
+        {
+            Console.WriteLine("Too low!");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Invalid input");
+    }
+}
